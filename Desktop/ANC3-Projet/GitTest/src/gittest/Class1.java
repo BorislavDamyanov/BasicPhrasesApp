cf49216c621aa7f82d0b44c050cecf446a8513f6
BorislavDamyanov <BorislavDamyanov@192.168.1.5>
2016-04-24 20:02:44 +0200

    Version initiale 1

--- Desktop/ANC3-Projet/GitTest/src/gittest/Class1.java
+++ Desktop/ANC3-Projet/GitTest/src/gittest/Class1.java
@@ -9,13 +9,6 @@ package gittest;
  *
  * @author BorislavDamyanov
  */
-public class GitTest {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-    }
+public class Class1 {
     
 }
